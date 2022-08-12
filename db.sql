3b55b8ee527630a9b41692905a1ba8167d6d4d6c
Othman Turki <othmanturki.dev@gmail.com>
2022-08-12 11:16:50 +0100

    Added DigiTex Tables

--- db.sql
+++ db.sql
@@ -1,8 +1,8 @@
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
--- INITIALISATION
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
+-- -----------------------------------------------------------------------------
+-- -----------------------------------------------------------------------------
+--                            INITIALISATION
+-- -----------------------------------------------------------------------------
+-- -----------------------------------------------------------------------------
 
 CREATE TABLE operators(
     id INT NOT NULL AUTO_INCREMENT,
@@ -41,8 +41,8 @@ CREATE TABLE prod_lines(
     prod_line VARCHAR(255) NOT NULL,
     operator_reg_num VARCHAR(255) NOT NULL,
     monitor_reg_num VARCHAR(255) NOT NULL,
-    machine_ref VARCHAR(255) DEFAULT NULL,
-    digitex VARCHAR(255) DEFAULT NULL,
+    machine_ref VARCHAR(255) NOT NULL DEFAULT '',
+    digitex VARCHAR(255) NOT NULL DEFAULT '',
     PRIMARY KEY(id),
     FOREIGN KEY (operator_reg_num) REFERENCES operators(reg_num),
     FOREIGN KEY (monitor_reg_num) REFERENCES monitors(reg_num),
@@ -50,11 +50,11 @@ CREATE TABLE prod_lines(
 );
 
 
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
--- PRODUCTION
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
+-- -----------------------------------------------------------------------------
+-- -----------------------------------------------------------------------------
+--                              PRODUCTION
+-- -----------------------------------------------------------------------------
+-- -----------------------------------------------------------------------------
 
 CREATE TABLE fab_orders(
     id INT NOT NULL AUTO_INCREMENT,
@@ -66,7 +66,7 @@ CREATE TABLE fab_orders(
     fo_status VARCHAR(255) NOT NULL DEFAULT 'START',
     start_date VARCHAR(255) NOT NULL,
     end_date VARCHAR(255) NOT NULL DEFAULT '',
-    import_date DATE DEFAULT DATE_FORMAT(CURDATE(), '%d/%m/%Y'),
+    import_date DATE DEFAULT CURDATE(),
     import_time TIME DEFAULT CURTIME(),
     PRIMARY KEY(id)
 );
@@ -80,7 +80,7 @@ CREATE TABLE packets(
     size VARCHAR(255) NOT NULL,
     quantity VARCHAR(255) NOT NULL,
     fab_order_id INT NOT NULL,
-    import_date DATE DEFAULT DATE_FORMAT(CURDATE(), '%d/%m/%Y'),
+    import_date DATE DEFAULT CURDATE(),
     import_time TIME DEFAULT CURTIME(),
     PRIMARY KEY(id),
     FOREIGN KEY (fab_order_id) REFERENCES fab_orders(id) ON DELETE CASCADE
@@ -101,9 +101,9 @@ CREATE TABLE gamut(
     designation VARCHAR(255) NOT NULL,
     unit_time VARCHAR(255) NOT NULL,
     qte_h VARCHAR(255) NOT NULL,
-    machine_ref VARCHAR(255) DEFAULT NULL,
-    digitex VARCHAR(255) DEFAULT NULL,
-    import_date DATE DEFAULT DATE_FORMAT(CURDATE(), '%d/%m/%Y'),
+    machine_ref VARCHAR(255) NOT NULL DEFAULT '',
+    digitex VARCHAR(255) NOT NULL DEFAULT '',
+    import_date DATE DEFAULT CURDATE(),
     import_time TIME DEFAULT CURTIME(),
     PRIMARY KEY(id),
     FOREIGN KEY (fab_order_id) REFERENCES fab_orders(id) ON DELETE CASCADE,
@@ -112,9 +112,90 @@ CREATE TABLE gamut(
 );
 
 
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
--- DIGITEX
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
+-- -----------------------------------------------------------------------------
+-- -----------------------------------------------------------------------------
+--                                DIGITEX
+-- -----------------------------------------------------------------------------
+-- -----------------------------------------------------------------------------
 
+CREATE TABLE pack_operations(
+    id INT NOT NULL AUTO_INCREMENT,
+    pack_num VARCHAR(255) NOT NULL,
+    operation_code VARCHAR(255) NOT NULL,
+    designation VARCHAR(255) NOT NULL,
+    unit_time VARCHAR(255) NOT NULL,
+    pack_qte VARCHAR(255) NOT NULL,
+    operator_reg_num VARCHAR(255) NOT NULL,
+    machine_ref VARCHAR(255) NOT NULL,
+    digitex VARCHAR(255) NOT NULL,
+    cur_date DATE DEFAULT CURDATE(),
+    cur_time TIME DEFAULT CURTIME(),
+    PRIMARY KEY(id),
+    FOREIGN KEY (operator_reg_num) REFERENCES operators(reg_num),
+);
+
+CREATE TABLE presences(
+    id INT NOT NULL AUTO_INCREMENT,
+    digitex VARCHAR(255) NOT NULL,
+    operator_reg_num VARCHAR(255) NOT NULL,
+    p_status VARCHAR(255) NOT NULL,
+    cur_date DATE DEFAULT CURDATE(),
+    cur_time TIME DEFAULT CURTIME(),
+    PRIMARY KEY(id),
+    FOREIGN KEY (operator_reg_num) REFERENCES operators(reg_num),
+    CHECK (p_status IN ('in', 'out'))
+);
+
+CREATE TABLE downtimes(
+    id INT NOT NULL AUTO_INCREMENT,
+    digitex VARCHAR(255) NOT NULL,
+    operator_reg_num VARCHAR(255) NOT NULL,
+    downtime INT NOT NULL,
+    cur_date DATE DEFAULT CURDATE(),
+    cur_time TIME DEFAULT CURTIME(),
+    PRIMARY KEY(id),
+    FOREIGN KEY (operator_reg_num) REFERENCES operators(reg_num),
+);
+
+CREATE TABLE op_performances(
+    id INT NOT NULL AUTO_INCREMENT,
+    operator_reg_num VARCHAR(255) NOT NULL,
+    production_time VARCHAR(255) NOT NULL,
+    downtime VARCHAR(255) NOT NULL,
+    presence_time VARCHAR(255) NOT NULL,
+    performance VARCHAR(255) NOT NULL,
+    cur_date DATE DEFAULT CURDATE(),
+    cur_time TIME DEFAULT CURTIME(),
+    PRIMARY KEY(id),
+    FOREIGN KEY (operator_reg_num) REFERENCES operators(reg_num)
+);
+
+CREATE TABLE notifications(
+    id INT NOT NULL AUTO_INCREMENT,
+    prod_line VARCHAR(255) NOT NULL,
+    digitex VARCHAR(255) NOT NULL,
+    operator_reg_num VARCHAR(255) NOT NULL,
+    monitor_reg_num VARCHAR(255) NOT NULL,
+    call_monitor VARCHAR(255) NOT NULL DEFAULT '',
+    monitor_arrival_time VARCHAR(255) NOT NULL DEFAULT '',
+    call_maintainer VARCHAR(255) NOT NULL DEFAULT '',
+    instant_call_maintainer VARCHAR(255) NOT NULL DEFAULT '',
+    maintainer_arrival_time VARCHAR(255) NOT NULL DEFAULT '',
+    PRIMARY KEY(id),
+    FOREIGN KEY (operator_reg_num) REFERENCES operators(reg_num),
+    FOREIGN KEY (monitor_reg_num) REFERENCES monitors(reg_num),
+);
+
+CREATE TABLE interventions(
+    id INT NOT NULL AUTO_INCREMENT,
+    maintainer_reg_num VARCHAR(255) NOT NULL,
+    intervention_time VARCHAR(255) NOT NULL DEFAULT '',
+    failure_type VARCHAR(255) NOT NULL DEFAULT '',
+    machine_ref VARCHAR(255) NOT NULL DEFAULT '',
+    spare_time VARCHAR(255) NOT NULL DEFAULT '',
+    cur_date DATE DEFAULT CURDATE(),
+    cur_time TIME DEFAULT CURTIME(),  -- DUPLICATION: check with Hela
+    PRIMARY KEY(id),
+    FOREIGN KEY (maintainer_reg_num) REFERENCES maintainer_reg_num(reg_num),
+    FOREIGN KEY (machine_ref) REFERENCES machines(ref)
+);
